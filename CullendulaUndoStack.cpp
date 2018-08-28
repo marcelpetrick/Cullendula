621243732a8daedfee538ee8d21dcedcb6c1982b
Marcel Petrick <mail@marcelpetrick.it>
2018-08-28 11:55:08 +0200

    CullendulaUndoStack: added husk for the API

--- CullendulaUndoStack.cpp
+++ CullendulaUndoStack.cpp
@@ -16,6 +16,38 @@ CullendulaUndoStack::CullendulaUndoStack()
 {
     qDebug() << "CullendulaUndoStack::CullendulaUndoStack()";
     m_container.reserve(10); // TODO
+
+    //! @todo
+}
+
+//----------------------------------------------------------------------------------
+
+void CullendulaUndoStack::push(const QString &from, const QString &to)
+{
+    //! @todo
+}
+
+//----------------------------------------------------------------------------------
+
+CullendulaUndoItem CullendulaUndoStack::undo()
+{
+    //! @todo
+    return CullendulaUndoItem();
+}
+
+//----------------------------------------------------------------------------------
+
+CullendulaUndoItem CullendulaUndoStack::redo()
+{
+    //! @todo
+    return CullendulaUndoItem();
+}
+
+//----------------------------------------------------------------------------------
+
+size_t CullendulaUndoStack::getSize()
+{
+    return static_cast<size_t>(m_container.size());
 }
 
 //----------------------------------------------------------------------------------
--- CullendulaUndoStack.h
+++ CullendulaUndoStack.h
@@ -12,7 +12,8 @@
 
 //----------------------------------------------------------------------------------
 
-class UndoItem
+//! @class Helper to store the "source, target"-tuples for the moved files.
+class CullendulaUndoItem
 {
 public:
     //! Quite simple: container for a pair of two paths (saved as String).
@@ -28,17 +29,27 @@ class CullendulaUndoStack
 public:
     CullendulaUndoStack();
 
-    // push: insert new pair (from, to)
+    // push: insert new pair (from, to); will be converted to CullendulaUndoItem
+    void push(QString const& from, QString const& to);
 
-    // pop: get last transaction
+    // will be replaced by "undo and redo" - was: pop: get last transaction
+    //! TODO
+    CullendulaUndoItem undo();
+    CullendulaUndoItem redo();
 
-    // get the current size; amount of items
+    //! Return the current size; amount of items
+    size_t getSize();
 
     // due to concerns while thinking about the API in combination with Redo, reading about the Command/Memento-Pattern is necessary ..
     // https://en.wikipedia.org/wiki/Command_pattern
     // https://en.wikipedia.org/wiki/Memento_pattern
 
 private:
-    //! container
-    QVector<UndoItem> m_container;
+    //! container: keeps the data
+    QVector<CullendulaUndoItem> m_container;
+
+    //! Keeps track of the pointer. In case of undo it is quite simple just to pop the last item.
+    //! Then redo would be enabled, because the pointer is set back by one.
+    //! Redo walks back the pointer by one
+    size_t m_currentItem = 0;
 };
